ce8fc7e0c2d97f19a6165ef705fa8f43cc33b6dc
Mahoo12138 <mahoo12138@outlook.com>
2022-07-09 15:26:52 +0800

    Button 组件继承原生属性

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -20,24 +20,27 @@ interface BaseButtonProps {
   href?: string;
   children?: React.ReactNode;
 }
+type NativeButtonProps = BaseButtonProps & React.ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & React.AnchorHTMLAttributes<HTMLElement>
+type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
 
-const Button: React.FC<BaseButtonProps> = (props) => {
-  const { btnType, disabled, size, children, href } = props;
+const Button: React.FC<ButtonProps> = (props) => {
+  const { btnType, disabled, size, children, href, className, ...restProps } = props;
 
-  const classes = classNames("btn", {
+  const classes = classNames("btn", className,{
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
     disabled: btnType === ButtonType.Link && disabled,
   });
   if (btnType === ButtonType.Link || href) {
     return (
-      <a className={classes} href={href}>
+      <a className={classes} href={href} {...restProps}>
         {children}
       </a>
     );
   } else {
     return (
-      <button className={classes} disabled={disabled}>
+      <button className={classes} disabled={disabled} {...restProps}>
         {children}
       </button>
     );
