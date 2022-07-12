dd1988586ae2d8fb10025b4a5377645d3a8531c0
Mahoo12138 <mahoo12138@outlook.com>
2022-07-12 13:17:21 +0800

    Menu 组件测试

---
+++ src/components/Menu/menu.test.tsx
@@ -0,0 +1,73 @@
+import {
+  fireEvent,
+  render,
+  RenderResult,
+  screen,
+} from "@testing-library/react";
+
+import Menu, { MenuProps } from "./menu";
+import MenuItem from "./menuItem";
+
+const testProps: MenuProps = {
+  defaultIndex: 0,
+  onSelect: jest.fn(),
+  className: "test",
+};
+
+const testVerProps: MenuProps = {
+  defaultIndex: 0,
+  mode: "vertical",
+};
+
+const generateMenu = (props: MenuProps) => {
+  return (
+    <Menu {...props}>
+      <MenuItem index={0}>active</MenuItem>
+      <MenuItem index={1} disabled>
+        disabled
+      </MenuItem>
+      <MenuItem index={2}>Click Item</MenuItem>
+    </Menu>
+  );
+};
+let wrapper: RenderResult,
+  menuElement: HTMLElement,
+  activeElement: HTMLElement,
+  disabledElement: HTMLElement;
+
+const setup = () => {
+  render(generateMenu(testProps));
+  menuElement = screen.getByTestId("test-menu");
+  activeElement = screen.getByText("active");
+  disabledElement = screen.getByText("disabled");
+};
+describe("Test Menu and MenuItem component", () => {
+  setup();
+  it("should render correct Menu and MenuItem based on default props", () => {
+    expect(menuElement).toBeInTheDocument();
+    expect(menuElement).toHaveClass("mahoo-menu test");
+    // eslint-disable-next-line testing-library/no-node-access
+    // expect(menuElement.getElementsByTagName("li")).toEqual(3);
+    const children = screen.getAllByRole('listitem')
+    expect(children.length).toEqual(3);
+    expect(activeElement).toHaveClass("menu-item is-active");
+    expect(disabledElement).toHaveClass('menu-item  is-disabled')
+  });
+  it("click items should change active and call the right callback", () => {
+    setup();
+    const thirdItem = screen.getByText(/Click Item/)
+    fireEvent.click(thirdItem)
+    expect(thirdItem).toHaveClass('is-active')
+    expect(activeElement).not.toHaveClass('is-active')
+    expect(testProps.onSelect).toHaveBeenCalledWith(2)
+
+    fireEvent.click(disabledElement)
+    expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
+
+  });
+  it("should render vertical mode when mode is set to vertical", () => {
+    render(generateMenu(testVerProps))
+    const menuVertical = screen.getByTestId('test-menu')
+    expect(menuVertical).toHaveClass('menu-vertical')
+  });
+});
