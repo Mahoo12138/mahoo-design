de538cfa86ea7f9004c7b36adbde44c32f7c72c8
Mahoo12138 <mahoo12138@outlook.com>
2022-07-14 16:30:59 +0800

    Menu 组件驱动测试

--- src/components/Menu/menu.test.tsx
+++ src/components/Menu/menu.test.tsx
@@ -3,42 +3,61 @@ import {
   render,
   RenderResult,
   screen,
+  waitFor
 } from "@testing-library/react";
 
 import Menu, { MenuProps } from "./menu";
 import MenuItem from "./menuItem";
+import SubMenu from "./subMenu";
 
 const testProps: MenuProps = {
-  defaultIndex: 0,
+  defaultIndex: "0",
   onSelect: jest.fn(),
   className: "test",
 };
 
 const testVerProps: MenuProps = {
-  defaultIndex: 0,
+  defaultIndex: "0",
   mode: "vertical",
 };
 
 const generateMenu2 = (props: MenuProps) => {
   return (
     <Menu {...props}>
-      <MenuItem index={0}>active</MenuItem>
-      <MenuItem index={1} disabled>
+      <MenuItem index={"0"}>active</MenuItem>
+      <MenuItem index={"1"} disabled>
         disabled
       </MenuItem>
-      <MenuItem index={2}>Click Item</MenuItem>
+      <MenuItem index={"2"}>Click Item</MenuItem>
+    </Menu>
+  );
+};
+const generateMenu3 = (props: MenuProps) => {
+  return (
+    <Menu {...props}>
+      <MenuItem>active</MenuItem>
+      <MenuItem disabled>disabled</MenuItem>
+      <MenuItem>Click Item</MenuItem>
+      <li>ListItem</li>
+      <SubMenu title={"子菜单"}>
+        <MenuItem>葡萄</MenuItem>
+        <MenuItem disabled>草莓</MenuItem>
+        <MenuItem>菠萝</MenuItem>
+      </SubMenu>
     </Menu>
   );
 };
 const generateMenu = (props: MenuProps) => {
   return (
     <Menu {...props}>
-      <MenuItem >active</MenuItem>
-      <MenuItem  disabled>
-        disabled
-      </MenuItem>
-      <MenuItem >Click Item</MenuItem>
-      <li>ListItem</li>
+      <MenuItem>active</MenuItem>
+      <MenuItem disabled>disabled</MenuItem>
+      <MenuItem>Click Item</MenuItem>
+      <SubMenu title={"子菜单"}>
+        <MenuItem>葡萄</MenuItem>
+        <MenuItem disabled>草莓</MenuItem>
+        <MenuItem>菠萝</MenuItem>
+      </SubMenu>
     </Menu>
   );
 };
@@ -47,11 +66,30 @@ let wrapper: RenderResult,
   activeElement: HTMLElement,
   disabledElement: HTMLElement;
 
+const createStyleFile = () => {
+  const cssFile: string = `
+    .mahoo-submenu{
+      display: none;
+    }
+    .mahoo-submenu.menu-opened{
+      display: block;
+    }
+  `;
+  const style = document.createElement("style");
+  style.type = "text/css";
+  style.innerHTML = cssFile;
+  return style
+};
+
 const setup = () => {
-  render(generateMenu(testProps));
+  const view = render(generateMenu(testProps));
   menuElement = screen.getByTestId("test-menu");
   activeElement = screen.getByText("active");
   disabledElement = screen.getByText("disabled");
+  // eslint-disable-next-line testing-library/no-container
+  view.container.append(createStyleFile())
+  // screen.
+  
 };
 describe("Test Menu and MenuItem component", () => {
   setup();
@@ -60,26 +98,45 @@ describe("Test Menu and MenuItem component", () => {
     expect(menuElement).toHaveClass("mahoo-menu test");
     // eslint-disable-next-line testing-library/no-node-access
     // expect(menuElement.getElementsByTagName("li")).toEqual(3);
-    const children = screen.getAllByRole('listitem')
-    expect(children.length).toEqual(3);
+    const children = screen.getAllByRole("listitem");
+    expect(children.length).toEqual(4);
     expect(activeElement).toHaveClass("menu-item is-active");
-    expect(disabledElement).toHaveClass('menu-item  is-disabled')
+    expect(disabledElement).toHaveClass("menu-item  is-disabled");
   });
   it("click items should change active and call the right callback", () => {
     setup();
-    const thirdItem = screen.getByText(/Click Item/)
-    fireEvent.click(thirdItem)
-    expect(thirdItem).toHaveClass('is-active')
-    expect(activeElement).not.toHaveClass('is-active')
-    expect(testProps.onSelect).toHaveBeenCalledWith(2)
+    const thirdItem = screen.getByText(/Click Item/);
+    fireEvent.click(thirdItem);
+    expect(thirdItem).toHaveClass("is-active");
+    expect(activeElement).not.toHaveClass("is-active");
+    expect(testProps.onSelect).toHaveBeenCalledWith("2");
 
-    fireEvent.click(disabledElement)
-    expect(testProps.onSelect).not.toHaveBeenCalledWith(1)
-
+    fireEvent.click(disabledElement);
+    expect(testProps.onSelect).not.toHaveBeenCalledWith("1");
   });
   it("should render vertical mode when mode is set to vertical", () => {
-    render(generateMenu(testVerProps))
-    const menuVertical = screen.getByTestId('test-menu')
-    expect(menuVertical).toHaveClass('menu-vertical')
+    render(generateMenu(testVerProps));
+    const menuVertical = screen.getByTestId("test-menu");
+    expect(menuVertical).toHaveClass("menu-vertical");
+  });
+  it("should show dropdown items when hover on subMenu", async () => {
+    setup();
+    expect(screen.queryByText("葡萄")).not.toBeVisible();
+    const dropdownElement = screen.getByText('子菜单');
+    fireEvent.mouseEnter(dropdownElement)
+    // 异步操作
+    await waitFor(()=>{
+      expect(screen.queryByText('葡萄')).toBeVisible()
+    })
+
+    fireEvent.click(screen.getByText('葡萄'))
+
+    expect(testProps.onSelect).toHaveBeenCalledWith('3-0')
+
+    fireEvent.mouseLeave(dropdownElement)
+
+    await waitFor(()=>{
+      expect(screen.queryByText('葡萄')).not.toBeVisible()
+    })
   });
 });
